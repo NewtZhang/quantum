2086566f54a056ceb5ba1d021a9f452664702d5d
NewtZhang <3210103414@zju.edu.cn>
2023-09-27 23:17:26 +0800

    create registers

--- Quantum-Optimization/code/first_quantum_circuits.py
+++ Quantum-Optimization/code/first_quantum_circuits.py
@@ -1,2 +1,13 @@
 from qiskit import *
-# Create a Quantum Circuit acting on a quantum register of three qubits
+
+# a try on the first quantum circuit
+# print(QuantumCircuit(2, 2).draw('mpl', interactive = True))
+
+# some qunatum and classical registers
+qreg1 = QuantumRegister(size=2, name='qreg1')
+qreg2 = QuantumRegister(1, 'qreg2')
+creg = ClassicalRegister(1, 'oldschool')
+
+qc = QuantumCircuit(qreg1, creg, qreg2)
+
+print(qc.draw())
