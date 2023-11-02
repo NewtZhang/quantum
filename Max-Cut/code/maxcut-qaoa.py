9a605dbe83fb80945ca7ecda4bac7ed4e4f1564b
NewtZhang <3210103414@zju.edu.cn>
2023-11-02 23:35:49 +0800

    ok?

--- Max-Cut/code/maxcut-qaoa.py
+++ Max-Cut/code/maxcut-qaoa.py
@@ -1,39 +1,25 @@
+from qiskit.opflow import Z,I
+from qiskit.algorithms import QAOA
+
+H1 = (Z^Z^I^I) + (I^Z^I^Z) + (Z^I^Z^I)# Define Z_0Z_1
+
+# qaoa = QAOA()
+# circuit = qaoa.construct_circuit([1,2],H1)[0]
+# circuit.draw(output="mpl")
+# circuit.decompose().decompose().draw(output="mpl")
+
+qaoa = QAOA(reps = 5)
+circuit = qaoa.construct_circuit([1,2,3,4,5,6,7,8,9,10],H1)[0]
+circuit.decompose().decompose().draw(output="mpl")
+
 from qiskit.utils import algorithm_globals, QuantumInstance
 from qiskit import Aer
-from qiskit_algorithms.optimizers import COBYLA
-from qiskit.opflow import Z
-from qiskit_algorithms.minimum_eigensolvers import QAOA
-from qiskit_ibm_provider import IBMProvider
-
-H1 = Z^Z # Define Z_0Z_1
-qaoa = QAOA()
-
-seed = 1234
+from qiskit.algorithms.optimizers import COBYLA
+seed = 1024
 algorithm_globals.random_seed = seed
 quantum_instance = QuantumInstance(Aer.get_backend("aer_simulator"),
-seed_simulator=seed, seed_transpiler=seed,
-shots = 10)
+seed_simulator=seed, seed_transpiler=seed,shots = 10)
 qaoa = QAOA(optimizer = COBYLA(), quantum_instance=quantum_instance)
 result = qaoa.compute_minimum_eigenvalue(H1)
 print(result)
 
-My_token = 'c9f9e73ead2cd8e4a404105185a3abd681a91e9012bd5c77cfd4c07bcd265c32c7f7ff67d0d2a374ecdbf7c70a22f4482868ba92212d09900928adcaba224090' # Paste your token here
-
-IBMProvider.save_account(My_token, overwrite=True)
-
-provider = IBMProvider.load_account()
-program_id = "qaoa"
-H1 = Z^Z
-opt = COBYLA()
-reps = 1
-shots = 1024
-runtime_inputs = {
-"operator": H1,
-"reps": reps,
-"optimizer": opt,
-"initial_point": [0,0],
-"use_swap_strategies": False
-}
-options = {"backend_name": "ibmq_belem"}
-job = provider.runtime.run(program_id=program_id, options=options, inputs=runtime_inputs)
-#job = provider.runtime.job(program_id)
--- Max-Cut/code/maxcut.py
+++ Max-Cut/code/maxcut.py
@@ -15,139 +15,65 @@ import time
 # from qiskit_optimization.algorithms import MinimumEigenOptimizer
 
 
-# sum_time = 0.0
-# for i in range(10): 
-#     start_time = time.perf_counter()
+sum_time = 0.0
+for i in range(2): 
+    start_time = time.perf_counter()
 
-#     # Generating a graph of 4 nodes
+    # Generating a graph of 4 nodes
 
-#     n = 15  # Number of nodes in graph
-#     G = nx.Graph()
-#     G.add_nodes_from(np.arange(0, n, 1))
-#     elist = [(0, 2, 1.0), (0, 3, 1.0), (0, 4, 1.0), (0, 5, 1.0), (0, 9, 1.0), (1, 3, 1.0), (1, 4, 1.0), (1, 5, 1.0), 
-#             (2, 3, 1.0), (2, 4, 1.0), (2, 6, 1.0), (2, 7, 1.0), (4, 5, 1.0), (4, 6, 1.0), (5, 8, 1.0), (5, 9, 1.0), 
-#             (6, 7, 1.0), (6, 8, 1.0), (6, 9, 1.0), (7, 8, 1.0), (7, 10, 1.0), (7, 14, 1.0), (8, 12, 1.0), (8, 13, 1.0), 
-#             (8, 14, 1.0), (9, 10, 1.0), (9, 11, 1.0), (9, 13, 1.0), (10, 12, 1.0), (10, 13, 1.0), (10, 14, 1.0), (11, 13, 1.0), 
-#             (12, 13, 1.0), (12, 14, 1.0), (13, 14, 1.0)]
-#     # tuple is (i,j,weight) where (i,j) is the edge
-#     G.add_weighted_edges_from(elist)
+    n = 15  # Number of nodes in graph
+    G = nx.Graph()
+    G.add_nodes_from(np.arange(0, n, 1))
+    elist = [(0, 2, 1.0), (0, 3, 1.0), (0, 4, 1.0), (0, 5, 1.0), (0, 9, 1.0), (1, 3, 1.0), (1, 4, 1.0), (1, 5, 1.0), 
+            (2, 3, 1.0), (2, 4, 1.0), (2, 6, 1.0), (2, 7, 1.0), (4, 5, 1.0), (4, 6, 1.0), (5, 8, 1.0), (5, 9, 1.0), 
+            (6, 7, 1.0), (6, 8, 1.0), (6, 9, 1.0), (7, 8, 1.0), (7, 10, 1.0), (7, 14, 1.0), (8, 12, 1.0), (8, 13, 1.0), 
+            (8, 14, 1.0), (9, 10, 1.0), (9, 11, 1.0), (9, 13, 1.0), (10, 12, 1.0), (10, 13, 1.0), (10, 14, 1.0), (11, 13, 1.0), 
+            (12, 13, 1.0), (12, 14, 1.0), (13, 14, 1.0)]
+    # tuple is (i,j,weight) where (i,j) is the edge
+    G.add_weighted_edges_from(elist)
 
-#     colors = ["r" for node in G.nodes()]
-#     pos = nx.spring_layout(G)
-
-
-#     def draw_graph(G, colors, pos):
-#         default_axes = plt.axes(frameon=True)
-#         nx.draw_networkx(G, node_color=colors, node_size=600, alpha=0.8, ax=default_axes, pos=pos)
-#         edge_labels = nx.get_edge_attributes(G, "weight")
-#         nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
+    colors = ["r" for node in G.nodes()]
+    pos = nx.spring_layout(G)
 
 
-#     draw_graph(G, colors, pos)
-
-#     # Computing the weight matrix from the random graph
-#     w = np.zeros([n, n])
-#     for i in range(n):
-#         for j in range(n):
-#             temp = G.get_edge_data(i, j, default=0)
-#             if temp != 0:
-#                 w[i, j] = temp["weight"]
-#     print(w)
-
-#     best_cost_brute = 0
-#     for b in range(2**n):
-#         x = [int(t) for t in reversed(list(bin(b)[2:].zfill(n)))]
-#         cost = 0
-#         for i in range(n):
-#             for j in range(n):
-#                 cost = cost + w[i, j] * x[i] * (1 - x[j])
-#         if best_cost_brute < cost:
-#             best_cost_brute = cost
-#             xbest_brute = x
-#         print("case = " + str(x) + " cost = " + str(cost))
-
-#     colors = ["r" if xbest_brute[i] == 0 else "c" for i in range(n)]
-#     draw_graph(G, colors, pos)
-#     print("\nBest solution = " + str(xbest_brute) + " cost = " + str(best_cost_brute))
-
-#     end_time = time.perf_counter()
-#     elapsed_time = end_time - start_time
-
-#     #print(f"Elapsed time: {elapsed_time:.6f} seconds")
-#     sum_time += elapsed_time
-
-# print(f"Average time: {sum_time/10:.6f} seconds")
+    def draw_graph(G, colors, pos):
+        default_axes = plt.axes(frameon=True)
+        nx.draw_networkx(G, node_color=colors, node_size=600, alpha=0.8, ax=default_axes, pos=pos)
+        edge_labels = nx.get_edge_attributes(G, "weight")
+        nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
 
 
+    draw_graph(G, colors, pos)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-start_time = time.perf_counter()
-
-# Generating a graph of 4 nodes
-
-n = 15  # Number of nodes in graph
-G = nx.Graph()
-G.add_nodes_from(np.arange(0, n, 1))
-elist = [(0, 2, 1.0), (0, 3, 1.0), (0, 4, 1.0), (0, 5, 1.0), (0, 9, 1.0), (1, 3, 1.0), (1, 4, 1.0), (1, 5, 1.0), 
-        (2, 3, 1.0), (2, 4, 1.0), (2, 6, 1.0), (2, 7, 1.0), (4, 5, 1.0), (4, 6, 1.0), (5, 8, 1.0), (5, 9, 1.0), 
-        (6, 7, 1.0), (6, 8, 1.0), (6, 9, 1.0), (7, 8, 1.0), (7, 10, 1.0), (7, 14, 1.0), (8, 12, 1.0), (8, 13, 1.0), 
-        (8, 14, 1.0), (9, 10, 1.0), (9, 11, 1.0), (9, 13, 1.0), (10, 12, 1.0), (10, 13, 1.0), (10, 14, 1.0), (11, 13, 1.0), 
-        (12, 13, 1.0), (12, 14, 1.0), (13, 14, 1.0)]
-# tuple is (i,j,weight) where (i,j) is the edge
-G.add_weighted_edges_from(elist)
-
-colors = ["r" for node in G.nodes()]
-pos = nx.spring_layout(G)
-
-
-def draw_graph(G, colors, pos):
-    default_axes = plt.axes(frameon=True)
-    nx.draw_networkx(G, node_color=colors, node_size=600, alpha=0.8, ax=default_axes, pos=pos)
-    edge_labels = nx.get_edge_attributes(G, "weight")
-    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
-
-
-draw_graph(G, colors, pos)
-
-# Computing the weight matrix from the random graph
-w = np.zeros([n, n])
-for i in range(n):
-    for j in range(n):
-        temp = G.get_edge_data(i, j, default=0)
-        if temp != 0:
-            w[i, j] = temp["weight"]
-print(w)
-
-best_cost_brute = 0
-for b in range(2**n):
-    x = [int(t) for t in reversed(list(bin(b)[2:].zfill(n)))]
-    cost = 0
+    # Computing the weight matrix from the random graph
+    w = np.zeros([n, n])
     for i in range(n):
         for j in range(n):
-            cost = cost + w[i, j] * x[i] * (1 - x[j])
-    if best_cost_brute < cost:
-        best_cost_brute = cost
-        xbest_brute = x
-    print("case = " + str(x) + " cost = " + str(cost))
+            temp = G.get_edge_data(i, j, default=0)
+            if temp != 0:
+                w[i, j] = temp["weight"]
+    print(w)
 
-colors = ["r" if xbest_brute[i] == 0 else "c" for i in range(n)]
-draw_graph(G, colors, pos)
-print("\nBest solution = " + str(xbest_brute) + " cost = " + str(best_cost_brute))
+    best_cost_brute = 0
+    for b in range(2**n):
+        x = [int(t) for t in reversed(list(bin(b)[2:].zfill(n)))]
+        cost = 0
+        for i in range(n):
+            for j in range(n):
+                cost = cost + w[i, j] * x[i] * (1 - x[j])
+        if best_cost_brute < cost:
+            best_cost_brute = cost
+            xbest_brute = x
+        print("case = " + str(x) + " cost = " + str(cost))
 
-end_time = time.perf_counter()
-elapsed_time = end_time - start_time
+    colors = ["r" if xbest_brute[i] == 0 else "c" for i in range(n)]
+    draw_graph(G, colors, pos)
+    print("\nBest solution = " + str(xbest_brute) + " cost = " + str(best_cost_brute))
 
-#print(f"Elapsed time: {elapsed_time:.6f} seconds")
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+
+    #print(f"Elapsed time: {elapsed_time:.6f} seconds")
+    sum_time += elapsed_time
+
+print(f"Average time: {sum_time/2:.6f} seconds")
